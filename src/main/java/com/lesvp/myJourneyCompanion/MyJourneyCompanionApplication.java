b8db9847cd95aef9101ff078ec448f011d1d5081
RolletQuentin <rollet.quentin@orange.fr>
2023-10-11 18:20:09 +0200

    first commit

---
+++ src/main/java/com/lesvp/myJourneyCompanion/MyJourneyCompanionApplication.java
@@ -0,0 +1,13 @@
+package com.lesvp.myJourneyCompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJourneyCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJourneyCompanionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lesvp/myJourneyCompanion/MyJourneyCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lesvp.myJourneyCompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJourneyCompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
