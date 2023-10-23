c2d128968c8964f90205cd03b8764cbbf67a1a7b
Sky <costematt@cy-tech.fr>
2023-10-23 14:16:21 +0200

    Video games entity without VideoGameController

--- src/main/java/com/lesvp/myJourneyCompanion/controller/VideoGameController.java
+++ src/main/java/com/lesvp/myJourneyCompanion/controller/VideoGameController.java
@@ -1,31 +1,39 @@
 package com.lesvp.myJourneyCompanion.controller;
 
+import com.lesvp.myJourneyCompanion.model.user.VideoGame;
 import com.lesvp.myJourneyCompanion.service.VideoGameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class VideoGameController {
 
-    @Autowired
-    private VideoGameService videoGameService;
+    private final VideoGameService videoGameService;
 
-    @GetMapping("/videoGames")
-    public String showVideoGames(Model model) {
-        String videoGamesData = null;
+    public VideoGameController(VideoGameService videoGameService) {
+        this.videoGameService = videoGameService;
+    }
+
+    @GetMapping("/video-games")
+    public ResponseEntity<String> showVideoGames(Model model) {
         try {
-            videoGamesData = videoGameService.getVideoGames();
-        } catch (URISyntaxException | IOException | InterruptedException e) {
-            throw new RuntimeException(e);
+            var videoGamesData = videoGameService.getVideoGames();
+            model.addAttribute("videoGames", videoGamesData);
+        } catch (Throwable e) {
+            return ResponseEntity
+                    .internalServerError()
+                    .body(e.getMessage());
         }
 
-        model.addAttribute("videoGames", videoGamesData);
-
-        return "videoGames";
+        return ResponseEntity.ok("videoGames");
     }
 }
