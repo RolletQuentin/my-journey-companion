2a4df4bf48b31ab98b3481111d3ad7964fbce897
RolletQuentin <rollet.quentin@orange.fr>
2023-10-25 10:38:01 +0200

    link topten view to database

--- src/main/java/com/lesvp/myJourneyCompanion/repository/VideoGameRepository.java
+++ src/main/java/com/lesvp/myJourneyCompanion/repository/VideoGameRepository.java
@@ -4,10 +4,12 @@ import com.lesvp.myJourneyCompanion.model.VideoGame;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface VideoGameRepository extends JpaRepository<VideoGame, UUID> {
-    public VideoGame findByUuid(UUID uuid);
     public VideoGame findByName(String name);
+
+    public List<VideoGame> findAllByOrderByMarkDesc();
 }
